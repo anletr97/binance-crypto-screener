0b35b78aafe89526c3b810b334aaa30dce760664
An Lê <anletr97@gmail.com>
2021-03-01 11:18:08 +0700

    Revert "Edit log messge, Note book: read csv"
    This reverts commit 6246961fdabc5ebc55c5612735534f845af1.

--- binance_tasks.py
+++ binance_tasks.py
@@ -74,10 +74,6 @@ def crawl_24h_price():
                     writer.writerow(row)
                     symbol_cnt += 1
                     files.append(file)
-            msg = '\'' + file_name + \
-            '\' has been successfully created for {} symbols.'.format(
-                symbol_cnt)
-            utils.log(constant.P_CRAWL_LAST_24H_PRICE, True, msg)
             return files
         else:
             raise res.raise_for_status()
